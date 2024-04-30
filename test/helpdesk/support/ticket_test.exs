968ced7df82aa64a9fb444466524bc35316e7eaf
Albin Kocheril Chacko <8876252+albinkc@users.noreply.github.com>
2024-05-01 00:21:14 +0530

    test data layer

--- test/helpdesk/support/ticket_test.exs
+++ test/helpdesk/support/ticket_test.exs
@@ -37,4 +37,38 @@ defmodule Helpdesk.Support.TicketTest do
     assert {:ok, _} = Ecto.UUID.cast(ticket.id)
     assert ticket.subject == "My mouse won't click!"
   end
+
+  test "data layer" do
+    # Use this to pick up changes you've made to your code, or restart your session
+
+    require Ash.Query
+
+    for i <- 0..5 do
+      ticket =
+        Helpdesk.Support.Ticket
+        |> Ash.Changeset.for_create(:open, %{subject: "Issue #{i}"})
+        |> Ash.create!()
+
+      if rem(i, 2) == 0 do
+        ticket
+        |> Ash.Changeset.for_update(:close)
+        |> Ash.update!()
+      end
+    end
+
+    # Show the tickets where the subject contains "2"
+    Helpdesk.Support.Ticket
+    |> Ash.Query.filter(contains(subject, "2"))
+    |> Ash.read!()
+
+    # Show the tickets that are closed and their subject does not contain "4"
+    output =
+      Helpdesk.Support.Ticket
+      |> Ash.Query.filter(status == :closed and not contains(subject, "4"))
+      |> Ash.read!()
+
+    # |> Ash.first!(:id)
+
+    assert {:ok, _} = Ecto.UUID.cast(hd(output).id)
+  end
 end
